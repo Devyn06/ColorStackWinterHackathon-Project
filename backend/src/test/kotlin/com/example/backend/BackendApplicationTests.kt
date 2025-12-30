4bcb3c88eaadb0214480dee8d65adf0ddc436db5
Erick Gomez <erickgomez714@hotmail.com>
2025-12-29 20:12:24 -0800

    commented out test to compile without DB

--- backend/src/test/kotlin/com/example/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/example/backend/BackendApplicationTests.kt
@@ -1,12 +1,12 @@
 package com.example.backend
-
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
-	@Test
-    @Disabled("Waiting for database configuration") // REMOVE THIS AFTER DATABASE IS ADDED
+	//@Test 
+    //@Disabled("Waiting for database configuration")  REMOVE THIS AFTER DATABASE IS ADDED AND OTHER COMMENTS
     fun contextLoads() {
     }
 }
