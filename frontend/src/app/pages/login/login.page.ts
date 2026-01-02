375ee40b6e8ca5daca57996d809e068e5505185b
Erick Gomez <erickgomez714@hotmail.com>
2026-01-01 21:44:16 -0800

    Map API and Geolocation added

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonInput,IonItem,IonList,IonInputPasswordToggle,IonText,IonButton,IonBackButton,IonButtons} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonInput,IonItem,IonList,IonInputPasswordToggle,
+  IonText,IonButton,IonBackButton,IonButtons,NavController} from '@ionic/angular/standalone';
 import { Router} from '@angular/router';
 @Component({
   selector: 'app-login',
@@ -16,16 +17,21 @@ export class LoginPage implements OnInit {
   userPassword:string = ''
 
   // injects router
-  constructor(private router:Router) { }
+  constructor(private router:Router,private navController:NavController) { }
 
   ngOnInit() {
   }
-  onLogin(){
-    // send userEmail and userPassword to DB
+  async onLogin(){
+    // send userEmail and userPassword to DB, AUTHENTICATE LOGIN HERE (TEMP REDIRECTS USER TO USERHOME)
     console.log(this.userEmail);
     console.log(this.userPassword);
+    // prevents back function after being logged in
+    await this.navController.navigateRoot('/userhome',{
+      animated: true,
+      animationDirection: 'forward'
+    });
   }
-  
+  // redirects user to signup page
   goToSignUp(){
     this.router.navigate(['/signup']);
   }
