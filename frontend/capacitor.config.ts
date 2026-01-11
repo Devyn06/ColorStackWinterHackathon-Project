3114575cf1ce735ceb5c3a621f07efcce22d39b2
Erick Gomez <erickgomez714@hotmail.com>
2026-01-10 21:32:28 -0800

    Adjusted keyboard behavior on join code and added ride progress UI elements

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,12 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-
+import { KeyboardResize } from '@capacitor/keyboard';
 const config: CapacitorConfig = {
   appId: 'com.example.groupridingapp',
   appName: 'frontend',
   webDir: 'www',
   android: {
     path: '../android' 
+  },
+  plugins: {
+  Keyboard: {
+    resize: KeyboardResize.Body, 
   }
+}
 };
 
 export default config;
--- frontend/src/app/pages/userhome/userhome.page.ts
+++ frontend/src/app/pages/userhome/userhome.page.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, signal,model,ViewChild } from '@angular/core';
+import { Component, OnInit, signal,model,ViewChild,Renderer2, ElementRef,OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonCardContent,IonRow,IonCol,IonButton,IonIcon,IonCard,
-  IonCardTitle,IonCardHeader,IonGrid,IonSpinner,IonRange,IonInput,RangeCustomEvent,IonButtons,IonToolbar,IonToggle,IonLabel} from '@ionic/angular/standalone';
+  IonCardTitle,IonCardHeader,IonGrid,IonSpinner,IonRange,IonInput,RangeCustomEvent,IonButtons,IonToolbar,IonToggle,IonLabel,IonFooter} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { addCircleOutline, peopleOutline, arrowBack,warningOutline} from 'ionicons/icons';
 // API keys
@@ -10,6 +10,7 @@ import { environment } from '../../../environments/environment';
 // impor to display the map
 import { MapDisplayComponent } from '../../components/map-display/map-display.component';
 import { MenuScreenComponent } from '../../components/menu-screen/menu-screen.component';
+import { Keyboard } from '@capacitor/keyboard';
 // gets API key from file
 const mapKey = environment.mapsKey;
 
@@ -19,9 +20,10 @@ const mapKey = environment.mapsKey;
   styleUrls: ['./userhome.page.scss'],
   standalone: true,
   imports: [IonContent, CommonModule, FormsModule,IonCardContent,IonRow,IonCol,IonButton,
-    IonIcon,IonCard,IonCardTitle,IonCardHeader,IonGrid,IonSpinner,IonRange,IonToggle, MapDisplayComponent,IonInput,IonButtons,IonToolbar,MenuScreenComponent,IonLabel]
+    IonIcon,IonCard,IonCardTitle,IonCardHeader,IonGrid,IonSpinner,IonRange,IonToggle, MapDisplayComponent,IonInput,IonButtons,IonToolbar,MenuScreenComponent,IonLabel,IonFooter]
 })
 export class UserhomePage implements OnInit {
+
   //allows us to use functions defined in map component 
   @ViewChild('mapComp') mapComponent!: MapDisplayComponent;
 
@@ -29,43 +31,81 @@ export class UserhomePage implements OnInit {
   lastPin: {lat: number,lng:number} | null = null;
 
   mapReady = signal(false);
+
   // holds all toggles for each factor
   speedToggled:boolean = false;
   curveToggled:boolean = false;
   weatherToggled:boolean = false;
   lightToggled:boolean = false;
+  
   pinCheck = signal(true);
-
+  display = signal(true);
+  
   // holds value for circle drag slider
   sliderPercent:number = 1000;
 
+  // live time 
+  currentTime = signal<string>('');
+  private timer: any;
 
   onIonChange(event:RangeCustomEvent){
+
     const newRadius = event.detail.value as number;
     
     this.sliderPercent = newRadius*10;
+
     // checks if user placed a pin to update circle with pin
     if(this.lastPin){
       this.mapComponent.updateRideCircle(this.lastPin.lat,this.lastPin.lng,this.sliderPercent);
     }
+
     console.log(this.sliderPercent);
   }
-  // what the user will view changes the on screen "cards" 
-  currentView = signal<'selection' | 'group' | 'create' | 'join' | 'start'>('selection');
 
-  // holds group leaders code
+  // what the user will view changes the on screen "cards" 
+  currentView = signal<'selection' | 'group' | 'create' | 'join' | 'start' |'tracking'|'end'>('selection');
+  
+  // User Codes
   groupCode = signal('');
-  // holds user inputted code "writeable signal" 2 way binding
   userJoinCode = model('');
 
   //receieves signal from map component 
   onMapReady(isReady:boolean){
     this.mapReady.set(isReady);
   }
-  constructor() {
+
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {
     addIcons({ addCircleOutline, peopleOutline,arrowBack,warningOutline});
   }
+
+  ngOnDestroy() {
+    if (this.timer) clearInterval(this.timer);
+  }
+  
   ngOnInit() {
+    this.updateTime();
+    // Refresh the time every second
+    this.timer = setInterval(() => {
+      this.updateTime();
+      console.log(this.currentTime());
+    }, 1000);
+
+    // When keyboard shows, move it up
+    Keyboard.addListener('keyboardWillShow', (info) => {
+      const container = this.el.nativeElement.querySelector('.bottom-container');
+      if (container) {
+        this.renderer.setStyle(container, 'transform', `translateY(-${info.keyboardHeight}px)`);
+      }
+    });
+
+    // When keyboard hides, move it back down
+    Keyboard.addListener('keyboardWillHide', () => {
+      const container = this.el.nativeElement.querySelector('.bottom-container');
+      if (container) {
+        this.renderer.setStyle(container, 'transform', 'translateY(0)');
+      }
+    });
   }
 
   // send data to map component to create a pin with circle
@@ -74,19 +114,28 @@ export class UserhomePage implements OnInit {
     this.mapComponent.updateRideCircle(coords.lat,coords.lng,this.sliderPercent);
     this.pinCheck.set(false);
   }
-
-
+  
+  // code generator
   createGroup(){
-    //creates group code for user
     const code = Math.random().toString(36).substring(2,8).toUpperCase();
     this.groupCode.set(code);
-    //changes users view
     this.currentView.set('create');
   }
 
+  // gets Live time
+  updateTime() {
+    const now = new Date();
+    this.currentTime.set(now.toLocaleTimeString([], { 
+      hour: '2-digit', 
+      minute: '2-digit',
+      hour12: true 
+    }));
+  }
   // Card routes (what user can see)
   selectGroup(){this.currentView.set('group');}
-  goBack(){this.currentView.set('selection');}
+  goBack(){this.currentView.set('selection'); this.display.set(true);}
   joinGroup(){this.currentView.set('join');}
-  startSession(){this.currentView.set('start');}
+  startSession(){this.currentView.set('start'); this.display.set(true);}
+  confirm(){this.currentView.set('tracking'); this.display.set(false);}
+  endOptions(){this.currentView.set('end');}
 }
